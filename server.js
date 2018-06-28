716d9ebc8d37b1911036ea84faa68d6afa66db81
ttchengab <ttchengab@connect.ust.hk>
2018-06-28 16:11:25 +0800

    Dockerize the screenshot function

--- server.js
+++ server.js
@@ -1,59 +1,39 @@
 var http = require('http');
 var url = require('url');
-var flag = 0;
 const puppeteer = require('puppeteer');
+var flag = 0;
+const OUT_FILE = 'testSDK.html';
 function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
+
 function sc(){
   (async () => {
     var server = http.createServer(function (req, res){
       urlData = url.parse(req.url, true);
       action = urlData.pathname;
       res.writeHead (200,{'Content-Type':'text/html'});
-      if (req.method == "POST")
-      {
-        //res.end ("<h1>hello Node.js</h1>");
-       
+      if (req.method == "POST")  {
         flag = 1;
-        
-       
         console.log("Got Request!");
-    
       }
-      else
-      {
-    
-    
-        //res.end ("<h1>hello Node.js</h1>");
-        
-       // console.log(action);
-        //console.log(req.method);
-       // sc();
-      }
-    
-      
     })
     server.listen(3000);
     const browser = await puppeteer.launch({
-      headless: false
+      args: ['--no-sandbox']
+    //  headless: false
     });
-   
-
     const page = await browser.newPage();
     await page.setViewport({width: 5960, height: 4209})
-    await page.goto('file:///C:/Users/TimCheng/Desktop/Everest%20Intern/Puppeteer/testSDK.html', );
-    
+    await page.goto(`file://${process.cwd()}/${OUT_FILE}`);
+    console.log("hi");
     await timeout(10000);
-    
-    if(flag == 1)
-    {
-     
-      await page.screenshot({path: 'example.png'});
+    if(flag == 1){
+      await page.screenshot({path: '/home/pptruser/example.png'});
     }
     await timeout(1000);
     await browser.close();
-    
+    console.log("process finished");
   })();
 }
 
